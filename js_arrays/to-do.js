0d1c10ee63d2162c4672204803373a2540bb21b7
Ephraim <ephygtz@gmail.com>
2018-07-05 15:43:20 +0300

    Add delete functionality and refactored code to re-usable methods

--- js_arrays/to-do.js
+++ js_arrays/to-do.js
@@ -3,24 +3,36 @@
 let toDos = [];
 
 //ask the user for input
-let input = prompt("What do you want to do?\n 1. quit \n 2. list \n 3. new");
-let sum = 1;
+let input = prompt("What do you want to do?\n 1. quit \n 2. list \n 3. new \n 4. delete");
+// let sum = 1;
 while(input !== "1"){
 	//check users input
 	if(input === "2"){
-		// console.log(toDos);
-		
-		toDos.forEach(function(i){
-			console.log(sum +" " + i);
-			sum++;
-		});
+		listTodos();
 	} else if(input ==="3"){
-		let toDo = prompt("Enter a new toDo");
-		toDos.push(toDo);
-	} 
+		addTodo();
+	} else if(input === "4"){
+		deleteToDo();
+	}
 		//
-		input = prompt("What do you want to do next????? \n 1. quit \n 2. list \n 3. new");
-	
+		input = prompt("What do you want to do next????? \n 1. quit \n 2. list \n 3. new \n 4. delete");
+}
+function listTodos(){
+	// console.log(toDos);
+	toDos.forEach(function(item, index){
+		console.log(index +" :: " + item);
+	});
+}
+function addTodo(){
+	let toDo = prompt("Enter a new toDo");
+		toDos.push(toDo);
+		console.log("Added");
+}
+
+function deleteToDo(){
+	let indexToDelete = prompt("Enter index to delete");
+	toDos.splice(indexToDelete, 1);
+	console.log("deleted");
 }
 console.log("Good bye");
 
