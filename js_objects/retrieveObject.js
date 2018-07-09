8b90512aad8617b84161313ca208c2d80044baf7
Ephraim <ephygtz@gmail.com>
2018-07-09 17:00:30 +0300

    Add objects challenges

---
+++ js_objects/retrieveObject.js
@@ -0,0 +1,18 @@
+/*
+Write code to print the following
+	* Izoh
+	* Ephra
+	* Mufasa
+	* Favorite color is baby blue
+  from the following object
+*/
+
+var someObject = {
+	friends : [
+		{name : "Izoh"}, 
+		{name : "Ephra"}, 
+		{name : "Mufasa"}
+		], 
+	color : "baby blue", 
+	isEvil: true
+};
