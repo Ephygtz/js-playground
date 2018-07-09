6faa479b7e925a6d2a865ac6cabe09cd22de56d6
Ephraim <ephygtz@gmail.com>
2018-07-09 19:42:12 +0300

    forLoop solution - es5

--- js_objects/movieDb.js
+++ js_objects/movieDb.js
@@ -9,3 +9,27 @@
 
 			USE YOUR OWN MOVIES!
 */
+
+"use strict";
+const movieDb = [
+	{title : "The gods must be crazy", rating : 3.5, hasWatched : true},
+	{title : "GOT", rating : 5, hasWatched : true},
+	{title : "Rick and Morty", rating : 4.5, hasWatched : false},
+	{title : "Hacker", rating : 2, hasWatched : false},
+	{title : "DEn of Thieves", rating : 4.8, hasWatched : true},
+	{title : "12 Stronh", rating : 5, hasWatched : true}
+];
+
+console.log(movieDb);
+for(let i = 0; i < movieDb.length; i++){
+	//Start building the string
+	let output = "You have ";
+	//check if watched is true
+	if(movieDb[i].hasWatched){
+		output += " WATCHED ";
+	} else {
+		output += " HAVE NOT WATCHED ";
+	}
+	output += " \" " + movieDb[i].title + " \" - " + movieDb[i].rating + " stars.";
+	console.log(output);
+}
