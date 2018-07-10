e918d08bb82b55390d4de8b8d9276d48ed0c3d32
Ephraim <ephygtz@gmail.com>
2018-07-10 11:16:02 +0300

    movieDB challenge

--- js_objects/movieDb.js
+++ js_objects/movieDb.js
@@ -10,40 +10,5 @@
 			USE YOUR OWN MOVIES!
 */
 
-"use strict";
-const moviesDb = [
-	{title : "The gods must be crazy", rating : 3.5, hasWatched : true},
-	{title : "GOT", rating : 5, hasWatched : true},
-	{title : "Rick and Morty", rating : 4.5, hasWatched : false},
-	{title : "Hacker", rating : 2, hasWatched : false},
-	{title : "DEn of Thieves", rating : 4.8, hasWatched : true},
-	{title : "12 Stronh", rating : 5, hasWatched : true}
-];
 
-// console.log(moviesDb);
-//Using a forLoop
-for(let i = 0; i < moviesDb.length; i++){
-	//Start building the string
-	let output = "You have ";
-	//check if watched is true
-	if(moviesDb[i].hasWatched){
-		output += " WATCHED ";
-	} else {
-		output += " HAVE NOT WATCHED ";
-	}
-	output += " \" " + moviesDb[i].title + " \" - " + moviesDb[i].rating + " stars.";
-	console.log(output);
-}
-console.log("\n *********************** \n ");
 
-//Using a forEach loop
-moviesDb.forEach((movie) => {
-	let buildString = `You have `;
-	if(movie.hasWatched){
-		buildString += ` WATCHED `;
-	}else {
-		buildString += ` HAVE NOT WATCHED `;
-	}
-	buildString += ` \" ${movie.title} \" ${movie.rating} stars.`;
-	console.log(buildString);
-});
