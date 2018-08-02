0118f524521f9401e880599b2a6a415e8afb3e36
Ephraim <ephygtz@gmail.com>
2018-08-03 00:09:39 +0300

    Randomize color selection

--- Practice_projects/color_game/colorGame.js
+++ Practice_projects/color_game/colorGame.js
@@ -1,42 +1,51 @@
 "use strict";
-// let colors = [
-// 	"rgb(255, 0, 0)",
-// 	"rgb(0, 255, 0)",
-// 	"rgb(0, 0, 255)",
-// 	"rgb(0, 255, 255)",
-// 	"rgb(255, 255, 255)",
-// 	"rgb(255, 0, 255)",
-// ];
-let colors = function() {
-	let r = Math.floor(Math.random() * 255);
-	let g = Math.floor(Math.random() * 255);
-	let b = Math.floor(Math.random() * 255);
-
-	return `rgb(${r},${g},${b})`;
-}
-
-
-
+let colors = [
+	randomColors(), 
+	randomColors(), 
+	randomColors(), 
+	randomColors(), 
+	randomColors(), 
+	randomColors()
+];
 
 let squares = document.querySelectorAll(".square");
 let colorToGuess = document.querySelector("span");
-
+let display = document.querySelector(".display");
+let picked = colors[Math.floor(Math.random() * colors.length)];
+console.log(picked);
 // let selectColor = Math.floor(Math.random() * 5);
-colorToGuess.textContent = colors();
+colorToGuess.textContent = picked;
+// console.log(squares);
 
 for(let i = 0; i < squares.length; i++){
 	//Set colors on squares
+	squares[i].style.background = colors[i];
 	// let randInt = Math.floor(Math.random() * 5);
-	let rand = colors();
-	squares[i].style.background = rand;
 
-	//Event listener
+	// Event listener
 	squares[i].addEventListener("click", function(){
-		let clickedColor = squares[i].style.background;
-		if(clickedColor === rand){
-			alert("Perfect");
+		let clickedColor = squares[i].style.backgroundColor;
+		console.log(clickedColor);
+		if(clickedColor === picked){
+			changeColorOnCorrectGuess(clickedColor);
+			display.style.background = clickedColor;
 		} else {
 			this.style.background = "#000";
 		}
 	});
+}
+
+function randomColors() {
+	let r = Math.floor(Math.random() * 255);
+	let g = Math.floor(Math.random() * 255);
+	let b = Math.floor(Math.random() * 255);
+
+	return `rgb(${r}, ${g}, ${b})`;
+	// return colors[r];
+}
+
+function changeColorOnCorrectGuess(color){
+	for(let j = 0; j < squares.length; j++){
+		squares[j].style.background = color;
+	}
 }
