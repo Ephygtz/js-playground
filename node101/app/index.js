b12ff30c428054456afbb0a7c997c2cd0dcbd182
DennisMufasa <denny.muasa@gmail.com>
2018-08-18 14:12:25 +0300

    update

---
+++ node101/app/index.js
@@ -0,0 +1,33 @@
+/*npm init --createa a package.json file when writen  on console.
+This package.json is where the dependencies for the app are saved including any modules used
+This will initialize a new nodejs app from scratch.
+npm i express --save  will save any used modules to package.json file.*/
+
+//require express from package.json
+const express = require('express');
+//initializing the express app
+const app = express(); 
+
+app.get('/', function(req, res){
+    res.send("<h1>Hi there welcome to my assignment!</h1>");
+});
+/*the full colon is placed before the parameter to match with user's params.
+the other params without the : prefix remain the same in order to access the route */
+app.get('/r/:title/comments/:id/:file_name', function(req, res){
+    //in the callback fn the first parameter is request and second is response
+    console.log(req.params); //returns an object with all the request params matched
+    let request = req.params.title; //var to store the params to send response
+    res.send(`<h2>${request} has been selected!</h2>`); //sendig response as a h2 element
+
+});
+
+/*make sure this is the last route otherwise all routes will redirect to it
+this route is redirected to when a user enters any url thatis not routed on your server*/
+app.get('*', function(req, res){
+    res.send('404', '<h1>What are  you doing</h1>');
+    console.log('Wildcard route match');
+});
+
+app.listen('9090', function(){
+    console.log('server running on port 9090');
+});
---
+++ node101/assignment/animals.js
@@ -0,0 +1,61 @@
+//require the express module
+const express = require('express');
+
+//initializing the express app
+const app = express();
+
+//route for welcome page
+app.get('/', function(req, res){
+    //response for homepage
+    res.send(`<h1>Welcome to Home Page</h1>`);
+    //consoling amessage
+    console.log('request made from home page');
+
+});
+
+//route 1
+app.get('/speak/:name', function(req, res){
+    //consoling request parameters
+    // console.log(req.params);
+    //variable animal to store request parameter
+    let animal = req.params.name;
+
+    if(animal === 'pig'){
+        res.send(`<h1>${animal} says 'Oink'</h1>`);
+    }else if(animal === 'dog'){
+        res.send(`<h1>${animal} says 'Woof'</h1>`);
+    }else if(animal === 'cat'){
+        res.send(`<h1>${animal} says 'Meow'</h1>`);
+    }else{
+        res.send(`<h1>${animal} does not exist!</h1>`);
+        }
+        console.log('request made from /speak/:');
+});
+
+//route 2
+app.get('/repeat/:mes/:count', function(req, res){
+    //variables to store request parameters
+    let name = req.params.mes;
+    let count = req.params.count;
+    if(count <= 0){
+        res.send('<h2>What are you doing?!</h2>');
+    }else{
+        res.send(`${name} `.repeat(count));
+        
+        console.log('request made from /repeat/:/:');
+    }
+});
+
+/*always the last route. It returns a 404 when the url requested for is not on server */
+app.get('*', function(req, res){
+    //sending a response to client
+    res.status(404).send('<h1>Check your url. Are you high?!</h1>');
+    //consoling a message
+    console.log('404 error!');
+});
+
+//setting the port the app will listen to
+app.listen('6000', function(){
+    //consoling a connection message
+    console.log('server running on port 6000!'); //optional
+}); 
